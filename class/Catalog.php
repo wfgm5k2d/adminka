64bfd3584c1b8daa9cdbe695ff2c5f33d7d7313c
wfgm5k2d <nktpgrlv@gmail.com>
2020-06-24 01:24:58 +0700

    decomposition catalog

--- class/Catalog.php
+++ class/Catalog.php
@@ -1,13 +1,13 @@
 <?php
-class Catalog{
+class ACCatalog{
 
     /**
      * @param $parent
      * @return array
      */
-    public static function getCat($parent)
+    public static function getCat()
 	{
-		$arItems = ACDatabase::getAll("SELECT * FROM catalog WHERE parent=?", $parent);
+		$arItems = ACDatabase::getAll("SELECT * FROM catalog");
 
         if(!empty($arItems))
             return $arItems;
@@ -19,9 +19,9 @@ class Catalog{
      * @param $id
      * @return mixed
      */
-    public static function getCatUrl($id)
+    public static function getParentUrl($id)
 	{
-		$arItems = ACDatabase::getRow("SELECT url FROM catalog WHERE id=?", $id);
+        $arItems = ACDatabase::getRow("SELECT url FROM catalog WHERE id=?", $id);
 
         if(!empty($arItems['url']))
             return $arItems['url'];
@@ -30,10 +30,24 @@ class Catalog{
 	}
 
     /**
+     * @param $id
+     * @return mixed
+     */
+    public static function getChildUrl($id)
+    {
+        $arItems = ACDatabase::getRow("SELECT url FROM ch_catalog WHERE id=?", $id);
+
+        if(!empty($arItems['url']))
+            return $arItems['url'];
+        else
+            return ACErrors::getError(1);
+    }
+
+    /**
      * @param $url
      * @return mixed
      */
-    public static function getCatId($url)
+    public static function getParentId($url)
 	{
 		$arItems = ACDatabase::getRow("SELECT id FROM catalog WHERE url=?", $url);
 
@@ -47,7 +61,21 @@ class Catalog{
      * @param $url
      * @return mixed
      */
-    public static function getCatName($url)
+    public static function getChildId($url)
+    {
+        $arItems = ACDatabase::getRow("SELECT id FROM ch_catalog WHERE url=?", $url);
+
+        if(!empty($arItems['id']))
+            return $arItems['id'];
+        else
+            return ACErrors::getError(1);
+    }
+
+    /**
+     * @param $url
+     * @return mixed
+     */
+    public static function getParentName($url)
 	{
 		$arItems = ACDatabase::getRow("SELECT name FROM catalog WHERE url=?", $url);
 
@@ -56,4 +84,18 @@ class Catalog{
         else
             return ACErrors::getError(1);
 	}
+
+    /**
+     * @param $url
+     * @return mixed
+     */
+    public static function getChildName($url)
+    {
+        $arItems = ACDatabase::getRow("SELECT name FROM ch_catalog WHERE url=?", $url);
+
+        if(!empty($arItems['name']))
+            return $arItems['name'];
+        else
+            return ACErrors::getError(1);
+    }
 }
--- index.php
+++ index.php
@@ -6,8 +6,4 @@ require $_SERVER['DOCUMENT_ROOT'].'/error/ACErrors.php';
 
 require $_SERVER['DOCUMENT_ROOT'].'/class/init.php';
 
-$obStatistic = new ACStatistic();
-
-$sIp = $obStatistic->getIP();
-
-$obStatistic->readIP($sIp);
+ACDatabase::set("DELETE FROM `catalog` WHERE `id` = ?", 2);
